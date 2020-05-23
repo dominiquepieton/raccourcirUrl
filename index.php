54af14d906c51693f7aaaaf470e446066fb74568
dominiquepieton <pietondominique@yahoo.fr>
2020-05-23 10:26:10 +0200

    architecture pour le depart

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="public/css/style.css">
+        <link rel="icon" href="public/img/favico.png" type="image/png">
+        <title>Url-raccourcisseur</title>
+    </head>
+    <body>
+        <section class="container">
+            <div class="content">
+                <header>
+                    <img class="logo" alt="logo du site" src="public/img/logo.png" />
+                </header>
+                <h1>url est trop longue ? raccourcissez-là ...</h1>
+                <h2>plus court que les autres se serait meilleur...</h2>
+                <form action="index.php" method="post">
+                    <input type="url" name ="url" placeholder="url à raccourcir..." />
+                    <input type="submit" value="soumettre" />
+                </form>
+            </div>
+        </section>
+        <section class="brands">
+            <div class="content">
+                <h3>les marques qui nous font confiance</h3>
+                <img src="public/img/1.png" alt="logo d'entreprise" class="picture">
+                <img src="public/img/2.png" alt="logo d'entreprise" class="picture">
+                <img src="public/img/3.png" alt="logo d'entreprise" class="picture">
+                <img src="public/img/4.png" alt="logo d'entreprise" class="picture">
+            </div>
+        </section>
+        <footer>
+            <img src="public/img/logo2.png" alt="logo du site" class="logo2">
+            <p class="text">2020 © DOM</p>
+            <a href="#" class="link">Contact</a>   <a href="#" class="link">À propos</a>
+        </footer>        
+    </body>
+</html>
